d45472968108898dbdf2cc5b2c5241a24912dc68
조하민 <chohamin0611@gmail.com>
2025-03-19 09:59:09 +0900

    3주차

--- Day03/src/App.tsx
+++ Day03/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 // import MapTest1 from './MapTest1'
-import MapTest2 from './MapTest2'
+// import MapTest2 from './MapTest2'
+// import PackingList from './ConditionJsx1'
+import MapPackingList from './ConditionJsx2'
 
 function App() {
 
   return (
     <>
       {/* <MapTest1 /> */}
-      <MapTest2 />
+      {/* <MapTest2 /> */}
+      {/* <PackingList /> */}
+      <MapPackingList />
     </>
   )
 }
