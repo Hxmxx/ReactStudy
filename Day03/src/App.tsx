624d54fdb9456f812eb0ff3160b26ccce0d9e452
조하민 <chohamin0611@gmail.com>
2025-03-19 09:31:18 +0900

    3주차

---
+++ Day03/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import MapTest1 from './MapTest1'
+import MapTest2 from './MapTest2'
+
+function App() {
+
+  return (
+    <>
+      {/* <MapTest1 /> */}
+      <MapTest2 />
+    </>
+  )
+}
+
+export default App
