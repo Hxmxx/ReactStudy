1292779e59d2e3b4405cb5e3f2e4e4db7580fd8e
조하민 <chohamin0611@gmail.com>
2025-03-26 10:01:20 +0900

    4주차

---
+++ Day04/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import InputText from './inputText/inputText'
+
+function App() {
+
+  // const msgList = [
+  //   "안녕하세요",
+  //   "2학년 학생입니다",
+  //   "리액트 수강생입니다"
+  // ];
+
+  // const msgCopy = [...msgList, "강의가 끝났습니다"];
+
+  // console.log(msgCopy);
+
+  return (
+    <>
+      <InputText />
+    </>
+  )
+}
+
+export default App
