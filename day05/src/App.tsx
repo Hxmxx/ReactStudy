ab505b8205f62dff701b983335fc89da2f7085de
조하민 <chohamin0611@gmail.com>
2025-04-02 09:29:17 +0900

    5주차

---
+++ day05/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+//import ProfileForm from './ProfileForm.tsx'
+import ProfileFormImmer from './PrifileFormImmer.tsx'
+
+function App() {
+
+  return (
+    <>
+        <ProfileFormImmer />
+    </>
+  )
+}
+
+export default App
